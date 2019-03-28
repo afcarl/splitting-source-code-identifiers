b360e943f0eff4cd1481e7681ac092d7a61abd16
bentrevett <bentrevett@gmail.com>
2019-03-28 18:37:15 +0000

    first pass of run

--- download.sh
+++ download.sh
@@ -3,5 +3,6 @@ gunzip repos2ids_v3.4_stats.csv.gz
 mkdir data
 mv repos2ids_v3.4_stats.csv data
 python preprocess.py
-cat all_examples{0..48}.json >> train_examples.json
-cat all_examples49.json >> test_examples.json
+cat data/all_examples{0..48}.json >> data/train_examples.json
+cat data/all_examples49.json >> data/test_examples.json
+
---
+++ run.py
@@ -0,0 +1,227 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import random
+import json
+import string
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--train_file', type=str, default='data/train_examples.json')
+parser.add_argument('--test_file', type=str, default='data/test_examples.json')
+parser.add_argument('--epochs', type=int, default=10)
+parser.add_argument('--embedding_dim', type=int, default=128)
+parser.add_argument('--hidden_dim', type=int, default=512)
+parser.add_argument('--n_layers', type=int, default=2)
+parser.add_argument('--unidirectional', action='store_true', default=False)
+parser.add_argument('--batch_size', type=int, default=512)
+parser.add_argument('--chunks', type=int, default=10)
+parser.add_argument('--print_every', type=int, default=100)
+parser.add_argument('--seed', type=int, default=1234)
+args = parser.parse_args()
+
+print(args)
+
+random.seed(args.seed)
+torch.manual_seed(args.seed)
+torch.backends.cudnn.deterministic = True
+
+vocab = set(string.ascii_lowercase + string.digits)
+
+vocab.add('<pad>')
+vocab.add('<unk>')
+
+char2idx = {c:i for i, c in enumerate(vocab)}
+idx2char = {i:c for i, c in enumerate(vocab)}
+
+class LSTMModel(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim,
+            bidirectional, n_layers, one_hot=False):
+        super().__init__()
+
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.rnn = nn.LSTM(embedding_dim, hidden_dim,
+                bidirectional=bidirectional, num_layers=n_layers)
+        self.fc = nn.Linear(hidden_dim * 2, output_dim)
+
+    def forward(self, x):
+
+        #x = [bsz, seq len]
+
+        e = self.embedding(x)
+
+        #e = [bsz, seq len, emb dim]
+
+        H, (_, _) = self.rnn(e.permute(1, 0, 2))
+
+        #H = [seq len, bsz, n directions * hid size]
+
+        o = self.fc(H.permute(1, 0, 2))
+
+        #o = [bsz, seq len, output dim]
+
+        return o
+
+
+model = LSTMModel(len(vocab), args.embedding_dim, args.hidden_dim, 1, not
+        args.unidirectional, args.n_layers)
+
+def file_iterator(path, batch_size, chunks):
+    """
+    Too many examples to store in memory, so this will load batch_size * chunks
+    examples into memory and then shuffle and yield them
+    """
+
+    examples = []
+
+    with open(path, 'r') as f:
+
+        for line in f:
+
+            examples.append(json.loads(line))
+
+            if len(examples) >= (batch_size * chunks):
+                random.shuffle(examples)
+                yield examples
+                examples = []
+
+    yield examples
+
+def pad(examples, pad_token):
+    """
+    Get list of list of examples, all different length and pad to the same
+    length
+    """
+
+    max_len = max([len(e) for e in examples])
+
+    for i, e in enumerate(examples):
+
+        while len(examples[i]) < max_len:
+            examples[i].append(pad_token)
+
+    return examples
+
+def tensor_iterator(examples, batch_size):
+    """
+    Get batch_size * chunks examples where each is a dict and yields a batch of
+    numericalized tensor inputs and outputs
+    """
+
+    x = [e['identifier'] for e in examples]
+    y = [e['target'] for e in examples]
+
+    x = [[char2idx[char] for char in e] for e in x]
+
+    n_batches = len(examples) // batch_size
+
+    for i in range(n_batches):
+
+        batch_x = pad(x[i*batch_size:(i+1)*batch_size],
+                pad_token=char2idx['<pad>'])
+        batch_y = pad(y[i*batch_size:(i+1)*batch_size],
+                pad_token=0)
+
+        yield torch.LongTensor(batch_x), torch.FloatTensor(batch_y)
+
+def train(model, path, device, criterion, optimizer):
+
+    model.train()
+
+    epoch_loss = 0
+
+    for i, examples in enumerate(file_iterator(path, args.batch_size, args.chunks), start=1):
+
+        for x, y in tensor_iterator(examples, args.batch_size):
+
+            x = x.to(device)
+            y = y.to(device)
+
+            optimizer.zero_grad()
+
+            predictions = model(x).squeeze(2)
+
+            mask = (x != char2idx['<pad>']).float()
+
+            del x
+
+            loss = criterion(predictions, y)
+
+            del predictions
+            del y
+
+            masked_loss = loss * mask
+
+            loss = masked_loss.sum() / mask.sum()
+
+            loss.backward()
+
+            optimizer.step()
+
+            epoch_loss += loss.item()
+
+        if i % args.print_every == 0:
+            print(f'Examples: {i*args.batch_size*args.chunks}, Loss: {epoch_loss/i:.3f}')
+
+    return epoch_loss / i, 0
+    
+
+def evaluate(model, path, device, criterion):
+
+    model.train()
+
+    epoch_loss = 0
+
+    with torch.no_grad():
+
+        for i, examples in enumerate(file_iterator(path, args.batch_size, args.chunks), start=1):
+
+            for (x, y) in tensor_iterator(examples, args.batch_size):
+
+                x = x.to(device)
+                y = y.to(device)
+
+                predictions = model(x).squeeze(2)
+
+                mask = (x != char2idx['<pad>']).float()
+
+                del x
+
+                loss = criterion(predictions, y)
+
+                del predictions
+                del y
+
+                masked_loss = loss * mask
+
+                loss = masked_loss.sum() / mask.sum()
+
+                epoch_loss += loss.item()
+
+            if i % args.print_every == 0:
+                print(f'Examples: {i*args.batch_size*args.chunks}, Loss: {epoch_loss/i:.3f}')
+
+    return epoch_loss / i, 0
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = model.to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+criterion = nn.BCEWithLogitsLoss(reduction='none')
+
+best_test_loss = float('inf')
+
+for epoch in range(1, args.epochs+1):
+
+    train_loss, train_f1 = train(model, args.train_file, device, criterion, optimizer)
+    test_loss, test_f1 = evaluate(model, args.test_file, device, criterion)
+
+    print(f'Epoch {epoch}, Train Loss: {train_loss:.3f}, Test Loss: {test_loss:.3f}')
+
+    if test_loss < best_test_loss:
+        best_test_loss = test_loss
+        torch.save(model.state_dict(), 'model.pt')
